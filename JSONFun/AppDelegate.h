95fb5c7c296bd4a0318ac817d0d5c132ac178ecf
Eric Meyer <emeyer@8thlight.com>
2013-02-23 09:49:57 -0600

    Initial Commit

---
+++ JSONFun/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  JSONFun
+//
+//  Created by Eric Meyer on 2/23/13.
+//  Copyright (c) 2013 Eric Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ JSONFun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JSONFun
+//
+//  Created by Eric Meyer on 2/23/13.
+//  Copyright (c) 2013 Eric Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
